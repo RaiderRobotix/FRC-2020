3b006b325e889c6cbe5665ad2044eb78a676d927
K2S0 <varunchari@yahoo.com>
2019-11-13 19:16:16 -0500

    Extend our Command Class

--- src/main/java/com/raiderrobotix/_2020/commands/Teleop.java
+++ src/main/java/com/raiderrobotix/_2020/commands/Teleop.java
@@ -3,8 +3,6 @@ package com.raiderrobotix._2020.commands;
 import static com.raiderrobotix._2020.OperatorInterface.input;
 import static com.raiderrobotix._2020.subsystems.DriveBase.drives;
 
-import edu.wpi.first.wpilibj.command.Command;
-
 public final class Teleop extends Command {
 
 	public Teleop() {
@@ -19,6 +17,15 @@ public final class Teleop extends Command {
 	@Override
 	protected boolean isFinished() {
 		return false;
+	}
+
+	@Override
+	protected void initialize() {
+	}
+
+	@Override
+	protected void end() {
+		drives.setSpeed(0);
 	}
 
 }
