8098f3cdfff9e5c16ef3750ec1aa96cdb8487b99
hybras <24651269+hybras@users.noreply.github.com>
2019-12-30 14:53:31 -0500

    Make primitive fields const

--- src/main/java/com/raiderrobotix/_2020/util/Vision.kt
+++ src/main/java/com/raiderrobotix/_2020/util/Vision.kt
@@ -4,13 +4,12 @@ import edu.wpi.first.cameraserver.CameraServer
 
 object Vision {
 	
-	
 	private val cameraServer = CameraServer.getInstance()
 	private val camera = cameraServer.startAutomaticCapture("FrontCam", 0)
 	
-	private val img_height = 180
-	private val img_width = 320
-	private val fps = 30
+	private const val img_height = 180
+	private const val img_width = 320
+	private const val fps = 30
 	
 	init {
 		camera.setResolution(img_width, img_height)
