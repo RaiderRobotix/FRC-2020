0a74ce4dbe98df5ba1d59e43f556bd450dcb0667
hybras <24651269+hybras@users.noreply.github.com>
2020-01-14 10:52:22 -0500

    Use periodic funcion as method body

--- src/main/kotlin/com/raiderrobotix/_2020/commands/Teleop.kt
+++ src/main/kotlin/com/raiderrobotix/_2020/commands/Teleop.kt
@@ -2,15 +2,11 @@ package com.raiderrobotix._2020.commands
 
 import com.raiderrobotix._2020.OperatorInterface
 import com.raiderrobotix._2020.subsystems.DriveBase
-import kotlinx.coroutines.coroutineScope
-import org.team2471.frc.lib.coroutines.meanlibLaunch
 import org.team2471.frc.lib.coroutines.periodic
 
-object Teleop: Action {
+object Teleop : Action {
 	
-	override suspend operator fun invoke() {
-			periodic(0.05) {
-				DriveBase.tankDrive(-OperatorInterface.leftY, -OperatorInterface.rightY)
-			}
+	override suspend operator fun invoke() = periodic(0.05) {
+		DriveBase.tankDrive(-OperatorInterface.leftY, -OperatorInterface.rightY)
 	}
 }
