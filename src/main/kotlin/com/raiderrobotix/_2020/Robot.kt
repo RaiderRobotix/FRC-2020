602e3a5f9db8e305216f45d6332793f3a5b3f93b
hybras <24651269+hybras@users.noreply.github.com>
2020-01-19 10:41:57 -0500

    Add dummy ultrasound code

--- src/main/kotlin/com/raiderrobotix/_2020/Robot.kt
+++ src/main/kotlin/com/raiderrobotix/_2020/Robot.kt
@@ -1,6 +1,9 @@
 package com.raiderrobotix._2020
 
 import com.raiderrobotix._2020.commands.Teleop
+import com.raiderrobotix._2020.util.Ultra
+import kotlinx.coroutines.GlobalScope
+import org.team2471.frc.lib.coroutines.meanlibLaunch
 import org.team2471.frc.lib.framework.RobotProgram
 import org.team2471.frc.lib.framework.initializeWpilib
 import org.team2471.frc.lib.framework.runRobotProgram
@@ -9,6 +12,7 @@ object Robot : RobotProgram {
 	
 	init {
 		// Vision
+		Ultra
 	}
 	
 	@JvmStatic
@@ -20,6 +24,12 @@ object Robot : RobotProgram {
 	
 	override suspend fun teleop() {
 		Teleop()
+	}
+	
+	override fun comms() {
+		GlobalScope.meanlibLaunch {
+			Ultra.updateDistance()
+		}
 	}
 	
 }
