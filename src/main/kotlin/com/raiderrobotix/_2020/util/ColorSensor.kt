9af7157d35a34b7d1ed82a66330b8287a730e9a5
hybras <24651269+hybras@users.noreply.github.com>
2020-01-29 18:13:22 -0500

    Detect section of color wheel using distance

--- src/main/kotlin/com/raiderrobotix/_2020/util/ColorSensor.kt
+++ src/main/kotlin/com/raiderrobotix/_2020/util/ColorSensor.kt
@@ -2,16 +2,31 @@ package com.raiderrobotix._2020.util
 
 import com.revrobotics.ColorSensorV3
 import edu.wpi.first.wpilibj.I2C
-import edu.wpi.first.wpilibj.util.Color
-
-import org.team2471.frc.lib.coroutines.periodic
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
+import edu.wpi.first.wpilibj.util.Color
+import org.team2471.frc.lib.coroutines.periodic
+import kotlin.math.hypot
 
-
-
-
-val colorSensor = ColorSensorV3(I2C.Port.kOnboard)
+val sensor = ColorSensorV3(I2C.Port.kOnboard)
 
 suspend fun printColor() = periodic {
-	SmartDashboard.putString("Color",colorSensor.color.toString())
+	SmartDashboard.putString("Color", sensor.color.toString())
 }
+
+enum class WheelColor(val color: Color) {
+	Red(Color(1.0, 0.0, 0.0)),
+	Green(Color(0.0, 1.0, 0.0)),
+	Cyan(Color(0.0, 1.0, 1.0)),
+	Yellow(Color(1.0, 1.0, 0.0));
+	
+	companion object {
+		val color: WheelColor
+			get() {
+				val color = sensor.color
+				return values().toList().minBy {
+					hypot(hypot((color.green - it.color.green), (color.red - it.color.red)), (color.blue - it.color.blue))
+				}!!
+			}
+	}
+}
+
