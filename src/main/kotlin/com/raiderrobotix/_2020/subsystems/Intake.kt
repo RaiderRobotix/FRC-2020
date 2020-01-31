8680b720005199d707a0c02a07055f869ae05b94
hybras <24651269+hybras@users.noreply.github.com>
2020-01-31 15:50:44 -0500

    Add Intake.kt

---
+++ src/main/kotlin/com/raiderrobotix/_2020/subsystems/Intake.kt
@@ -0,0 +1,25 @@
+package com.raiderrobotix._2020.subsystems
+
+import edu.wpi.first.wpilibj.Spark
+import org.team2471.frc.lib.framework.Subsystem
+
+
+object Intake : Subsystem("Intake") {
+	private const val topChannel = 3
+	private const val bottomChannel = 2
+	
+	private val upper = Spark(topChannel)
+	private val lower = Spark(bottomChannel)
+	
+	init {
+		lower.inverted = true
+	}
+	
+	var speed: Double
+		set(it) {
+			upper.set(it)
+			lower.set(it)
+		}
+		get() = (upper.get() + lower.get()) / 2
+	
+}
