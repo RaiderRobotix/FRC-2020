c085147812c530e9624fd708c7632aa8678eddb0
hybras <24651269+hybras@users.noreply.github.com>
2020-01-30 19:06:13 -0500

    Make shooter spin on button press

--- src/main/kotlin/com/raiderrobotix/_2020/OperatorInterface.kt
+++ src/main/kotlin/com/raiderrobotix/_2020/OperatorInterface.kt
@@ -1,9 +1,11 @@
 package com.raiderrobotix._2020
 
+import com.raiderrobotix._2020.subsystems.Shooter
 import edu.wpi.first.wpilibj.Joystick
 import edu.wpi.first.wpilibj.Sendable
 import edu.wpi.first.wpilibj.smartdashboard.SendableBuilder
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
+import org.ghrobotics.lib.wrappers.hid.mapControls
 import kotlin.math.abs
 
 object OperatorInterface : Sendable {
@@ -24,9 +26,23 @@ object OperatorInterface : Sendable {
 	private const val JOYSTICK_DEADBAND = 0.15
 	
 	// Joysticks
+	
 	val leftStick = Joystick(LEFT_JOYSTICK_PORT)
 	val rightStick = Joystick(RIGHT_JOYSTICK_PORT)
 	val operatorStick = Joystick(OPERATOR_JOYSTICK_PORT)
+	
+	init {
+		operatorStick.mapControls {
+			button(3) {
+				changeOff {
+					Shooter.speed = 0.5
+				}
+				changeOff {
+					Shooter.speed = 0.0
+				}
+			}
+		}
+	}
 	
 	/**
 	 * Down on Joystick is positive, up is negative
