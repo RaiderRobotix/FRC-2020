087d3d2ca325d910e4cfa9d5b80c2f959427e2ab
hybras <24651269+hybras@users.noreply.github.com>
2020-02-02 11:53:15 -0500

    Launch operatorControl inside its own coroutine

--- src/main/kotlin/com/raiderrobotix/_2020/commands/Teleop.kt
+++ src/main/kotlin/com/raiderrobotix/_2020/commands/Teleop.kt
@@ -2,14 +2,21 @@ package com.raiderrobotix._2020.commands
 
 import com.raiderrobotix._2020.OperatorInterface
 import com.raiderrobotix._2020.subsystems.DriveBase
+import kotlinx.coroutines.GlobalScope
+import org.team2471.frc.lib.coroutines.meanlibLaunch
 import org.team2471.frc.lib.coroutines.periodic
 import org.team2471.frc.lib.framework.use
 
-suspend fun operatorControl() = use(DriveBase) {
-	periodic {
-		DriveBase.tankDrive(
-			leftSpeed = -OperatorInterface.leftY,
-			rightSpeed = -OperatorInterface.rightY
-		)
+suspend fun operatorControl() = GlobalScope.meanlibLaunch {
+	println("Entered mean Launch")
+	use(DriveBase) {
+		println("USING DRIVEBASE")
+		periodic {
+			println("periodic USING DRIVEBASE")
+			DriveBase.tankDrive(
+				leftSpeed = -OperatorInterface.leftY,
+				rightSpeed = -OperatorInterface.rightY
+			)
+		}
 	}
 }
