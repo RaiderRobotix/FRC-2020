5e87482f26cad307cce4e72d5229eaa07c6f4b56
hybras <24651269+hybras@users.noreply.github.com>
2020-02-02 12:21:07 -0500

    Remove debugging prints

--- src/main/kotlin/com/raiderrobotix/_2020/commands/Teleop.kt
+++ src/main/kotlin/com/raiderrobotix/_2020/commands/Teleop.kt
@@ -2,21 +2,24 @@ package com.raiderrobotix._2020.commands
 
 import com.raiderrobotix._2020.OperatorInterface
 import com.raiderrobotix._2020.subsystems.DriveBase
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.GlobalScope
 import org.team2471.frc.lib.coroutines.meanlibLaunch
 import org.team2471.frc.lib.coroutines.periodic
 import org.team2471.frc.lib.framework.use
 
 suspend fun operatorControl() = GlobalScope.meanlibLaunch {
-	println("Entered mean Launch")
 	use(DriveBase) {
-		println("USING DRIVEBASE")
-		periodic {
-			println("periodic USING DRIVEBASE")
-			DriveBase.tankDrive(
-				leftSpeed = -OperatorInterface.leftY,
-				rightSpeed = -OperatorInterface.rightY
-			)
+		try {
+			periodic {
+				DriveBase.tankDrive(
+					leftSpeed = -OperatorInterface.leftY,
+					rightSpeed = -OperatorInterface.rightY
+				)
+			}
+		} catch (c: CancellationException) {
+			DriveBase.speed = 0.0
 		}
+		
 	}
 }
