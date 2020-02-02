831891aa78d22260a3b45290ea382a03ae91dfca
hybras <24651269+hybras@users.noreply.github.com>
2020-02-02 11:52:05 -0500

    Add outer Intake.kt roller

--- src/main/kotlin/com/raiderrobotix/_2020/subsystems/Intake.kt
+++ src/main/kotlin/com/raiderrobotix/_2020/subsystems/Intake.kt
@@ -5,13 +5,15 @@ import org.team2471.frc.lib.framework.Subsystem
 
 object Intake : Subsystem("Shooter") {
 	private const val topChannel = 2
-	private const val bottomChannel = 3 // TODO
+	private const val bottomChannel = 3
+	private const val outerChannel = 4
 	
 	private val upper = Spark(topChannel)
 	private val lower = Spark(bottomChannel)
+	private val outer = Spark(outerChannel)
 	
 	init {
-        lower.inverted = true
+		lower.inverted = true
 	}
 	
 	var speed: Double
@@ -20,5 +22,11 @@ object Intake : Subsystem("Shooter") {
 			lower.set(it)
 		}
 		get() = (upper.get() + lower.get()) / 2
+	
+	var mouthSpeed: Double
+		set(it) {
+			outer.set(it)
+		}
+		get() = outer.get()
 	
 }
