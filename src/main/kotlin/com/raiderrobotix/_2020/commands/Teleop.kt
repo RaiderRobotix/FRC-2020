8420860bf19abd8b2af117051b814225ee27892d
hybras <24651269+hybras@users.noreply.github.com>
2020-02-04 11:54:08 -0500

    Remove cooperative cancellation

--- src/main/kotlin/com/raiderrobotix/_2020/commands/Teleop.kt
+++ src/main/kotlin/com/raiderrobotix/_2020/commands/Teleop.kt
@@ -2,7 +2,6 @@ package com.raiderrobotix._2020.commands
 
 import com.raiderrobotix._2020.OperatorInterface
 import com.raiderrobotix._2020.subsystems.DriveBase
-import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.GlobalScope
 import org.team2471.frc.lib.coroutines.meanlibLaunch
 import org.team2471.frc.lib.coroutines.periodic
@@ -10,16 +9,11 @@ import org.team2471.frc.lib.framework.use
 
 suspend fun operatorControl() = GlobalScope.meanlibLaunch {
 	use(DriveBase) {
-		try {
-			periodic {
-				DriveBase.tankDrive(
-					leftSpeed = -OperatorInterface.leftY,
-					rightSpeed = -OperatorInterface.rightY
-				)
-			}
-		} catch (c: CancellationException) {
-			DriveBase.speed = 0.0
+		periodic {
+			DriveBase.tankDrive(
+				leftSpeed = -OperatorInterface.leftY,
+				rightSpeed = -OperatorInterface.rightY
+			)
 		}
-		
 	}
 }
