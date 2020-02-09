c036221dd4c425b8bcf0a4a3fad0c7f995bb4fd7
Hybras <24651269+hybras@users.noreply.github.com>
2020-02-09 11:47:54 -0500

    Cooperatively cancel testPrint

--- src/main/kotlin/com/raiderrobotix/_2020/commands/TestPrint.kt
+++ src/main/kotlin/com/raiderrobotix/_2020/commands/TestPrint.kt
@@ -2,18 +2,20 @@ package com.raiderrobotix._2020.commands
 
 import com.raiderrobotix._2020.OperatorInterface
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.isActive
 import org.team2471.frc.lib.coroutines.meanlibLaunch
 import org.team2471.frc.lib.coroutines.periodic
 
 fun testPrint() = GlobalScope.meanlibLaunch {
-    try {
         periodic(period = 0.25) {
-            print("periodic")
-            print("lY: ${OperatorInterface.rightY}")
-            print("rY: ${OperatorInterface.leftY}")
+            if (this@meanlibLaunch.isActive) {
+                print("periodic")
+                print("lY: ${OperatorInterface.rightY}")
+                print("rY: ${OperatorInterface.leftY}")
+            }
+            else {
+                stop()
+            }
         }
-    }finally {
-        println("Testing finished")
-    }
 
 }
