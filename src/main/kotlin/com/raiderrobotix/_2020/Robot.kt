2185c9869f92a2e819a7f29c4a70d94f82c3aa42
Hybras <24651269+hybras@users.noreply.github.com>
2020-02-12 15:54:29 -0500

    Enable Elevator default actions

--- src/main/kotlin/com/raiderrobotix/_2020/Robot.kt
+++ src/main/kotlin/com/raiderrobotix/_2020/Robot.kt
@@ -1,6 +1,7 @@
 package com.raiderrobotix._2020
 
 import com.raiderrobotix._2020.subsystems.DriveBase
+import com.raiderrobotix._2020.subsystems.Elevator
 import com.raiderrobotix._2020.util.updateDistance
 import org.team2471.frc.lib.framework.RobotProgram
 import org.team2471.frc.lib.framework.initializeWpilib
@@ -20,6 +21,7 @@ object Robot : RobotProgram {
 	
 	override suspend fun teleop() {
 		DriveBase.enable()
+		Elevator.enable()
 	}
 	
 	override suspend fun disable() {
