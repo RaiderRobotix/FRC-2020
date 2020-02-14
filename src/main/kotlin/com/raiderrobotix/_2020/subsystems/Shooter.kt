e0962dd55f62e7129bd5d35ae9e2209468af37c1
Hybras <24651269+hybras@users.noreply.github.com>
2020-02-14 15:42:45 -0500

    Group speedcontroller, add limit switch

--- src/main/kotlin/com/raiderrobotix/_2020/subsystems/Shooter.kt
+++ src/main/kotlin/com/raiderrobotix/_2020/subsystems/Shooter.kt
@@ -1,26 +1,33 @@
 package com.raiderrobotix._2020.subsystems
 
+import edu.wpi.first.wpilibj.DigitalInput
 import edu.wpi.first.wpilibj.Spark
+import edu.wpi.first.wpilibj.SpeedControllerGroup
 import org.team2471.frc.lib.framework.Subsystem
 
 object Shooter : Subsystem("Shooter") {
 	private const val topChannel = 1 // TODO
 	private const val bottomChannel = 0 // TODO
 	private const val cowlChannel = 8 // TODO
+	private const val limitDIO = 1 // TODO
 
-	private val top = Spark(topChannel)
-	private val bottom = Spark(bottomChannel)
 	private val cowl = Spark(cowlChannel)
+	private val group = SpeedControllerGroup(Spark(topChannel), Spark(bottomChannel))
+
+	private val limit = DigitalInput(limitDIO)
+	private const val limitEnabled = true
+
 
 	var speed: Double
 		set(it) {
-			top.set(it)
-			bottom.set(it)
+			if (limitEnabled && !limit.get()) {
+				group.set(it)
+			}
 		}
-		get() = (top.get() + bottom.get()) / 2
+		get() = group.get()
 
 	var cowlSpeed: Double
-		set(value) = let { cowl.speed = value }
+		set(value) { cowl.speed = value }
 		get() = cowl.speed
 
 	override fun reset() {
