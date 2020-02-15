8e30b168e2dbbe0f6ba241af93fa5ad6bc18dd51
hybras <24651269+hybras@users.noreply.github.com>
2020-02-15 15:54:35 -0500

    Set Ultrasound distance scaling

--- src/main/kotlin/com/raiderrobotix/_2020/Robot.kt
+++ src/main/kotlin/com/raiderrobotix/_2020/Robot.kt
@@ -24,6 +24,7 @@ object Robot : RobotProgram {
 		DriveBase.enable()
 		Elevator.enable()
 		printDistance()
+		updateDistance()
 	}
 	
 	override suspend fun disable() {
--- src/main/kotlin/com/raiderrobotix/_2020/util/Ultrasound.kt
+++ src/main/kotlin/com/raiderrobotix/_2020/util/Ultrasound.kt
@@ -6,7 +6,9 @@ import org.team2471.frc.lib.coroutines.periodic
 
 val ultrasound = AnalogInput(0)
 
+val distance get() = ultrasound.voltage * (24 / 0.21) // The conversion factor from voltage to inches
+
 suspend fun updateDistance() = periodic(0.05) {
-    SmartDashboard.putNumber("Ultrasound", ultrasound.voltage)
+	SmartDashboard.putNumber("Ultrasound", ultrasound.voltage)
 }
 
