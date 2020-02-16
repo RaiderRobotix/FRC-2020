46acd36478c5bee8042a9f421263c1d05718c226
hybras <24651269+hybras@users.noreply.github.com>
2020-02-16 12:15:17 -0500

    Finish TurnToColor function

--- src/main/kotlin/com/raiderrobotix/_2020/commands/TurnToColor.kt
+++ src/main/kotlin/com/raiderrobotix/_2020/commands/TurnToColor.kt
@@ -1,10 +1,14 @@
 package com.raiderrobotix._2020.commands
 
+import com.raiderrobotix._2020.subsystems.ColorWheel
 import com.raiderrobotix._2020.util.WheelColor
+import org.team2471.frc.lib.coroutines.periodic
 import org.team2471.frc.lib.framework.use
 
-suspend fun turnPanelToColor(color: WheelColor) = use {
-	while (WheelColor.color != color) {
-		// Turn wheel
+suspend fun turnPanelToColor(color: WheelColor = WheelColor.selectedColor) = use(ColorWheel) {
+	periodic {
+		if (WheelColor.color != color)
+			ColorWheel.motor.set(0.1)
+		else stop()
 	}
 }
