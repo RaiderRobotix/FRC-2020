be8d299384919e23e1a4167c54e5e862dc0ef60a
hybras <24651269+hybras@users.noreply.github.com>
2020-02-16 11:46:24 -0500

    Groups Elevator sparks into a group

--- src/main/kotlin/com/raiderrobotix/_2020/subsystems/Elevator.kt
+++ src/main/kotlin/com/raiderrobotix/_2020/subsystems/Elevator.kt
@@ -4,7 +4,8 @@ import com.raiderrobotix._2020.OperatorInterface
 import com.revrobotics.CANSparkMax
 import com.revrobotics.CANSparkMaxLowLevel
 import edu.wpi.first.wpilibj.Counter
-import edu.wpi.first.wpilibj.DigitalInput
+import edu.wpi.first.wpilibj.SpeedControllerGroup
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
 import org.team2471.frc.lib.coroutines.periodic
 import org.team2471.frc.lib.framework.Subsystem
 
@@ -16,18 +17,19 @@ object Elevator : Subsystem("Elevator") {
 	private val left = CANSparkMax(left_id, CANSparkMaxLowLevel.MotorType.kBrushless)
 	private val right = CANSparkMax(right_id, CANSparkMaxLowLevel.MotorType.kBrushless)
 	
+	private val group = SpeedControllerGroup(left, right)
 	private val left_encoder = left.encoder
 	private val right_encoder = right.encoder
-	
-	private const val limitDIO = 1 // TODO
-	private val counter = Counter(DigitalInput(limitDIO))
-	private var count = 0
-	private const val limitEnabled = false
+
+//	private const val limitDIO = 1 // TODO
+//	private val counter = Counter(DigitalInput(limitDIO))
+//	private var count = 0
+//	private const val limitEnabled = false
 	
 	operator fun Counter.invoke() = get()
 	
 	init {
-		left.follow(right, true)
+		left.inverted = true
 		left_encoder.position = 0.0
 		right_encoder.position = 0.0
 	}
@@ -38,14 +40,14 @@ object Elevator : Subsystem("Elevator") {
 	
 	var speed: Double
 		set(new_speed) {
-			right.set(
-				if (limitEnabled && count < counter()) {
-					count = counter()
-					new_speed.coerceAtLeast(0.0)
-				} else new_speed
-			)
+			group.set(
+//				if (limitEnabled && count < counter()) {
+//					count = counter()
+//					new_speed.coerceAtLeast(0.0)
+//				} else
+				new_speed)
 		}
-		get() = right.get()
+		get() = group.get()
 
 	override fun reset() {
 		speed = 0.0
@@ -53,6 +55,7 @@ object Elevator : Subsystem("Elevator") {
 
 	override suspend fun default() {
 		periodic {
+			SmartDashboard.putString("Voltage", "Left: ${left.busVoltage}, Right: ${right.busVoltage}")
 			speed = -OperatorInterface.operatorY
 		}
 	}
