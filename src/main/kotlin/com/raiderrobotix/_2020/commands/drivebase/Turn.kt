4234c2bf4c0b8d86fa4d3bac07868cb339b5d313
hybras <24651269+hybras@users.noreply.github.com>
2020-02-16 16:44:10 -0500

    Rewrite turn function

--- src/main/kotlin/com/raiderrobotix/_2020/commands/drivebase/Turn.kt
+++ src/main/kotlin/com/raiderrobotix/_2020/commands/drivebase/Turn.kt
@@ -1,7 +1,7 @@
 package com.raiderrobotix._2020.commands.drivebase
 
 import com.raiderrobotix._2020.subsystems.DriveBase
-import org.team2471.frc.lib.coroutines.delay
+import org.team2471.frc.lib.coroutines.periodic
 import org.team2471.frc.lib.framework.use
 import kotlin.math.abs
 
@@ -11,8 +11,13 @@ private const val TURN_TOLERANCE = 1.0 // TODO
  * @param speed must positive
  */
 suspend fun turn(angle: Double, speed: Double) = use(DriveBase) {
-	while (abs(DriveBase.gyroAngle - angle) >= TURN_TOLERANCE) {
-		DriveBase.tankDrive(speed, -speed)
-		delay(0.05)
+	val navX = DriveBase.navX
+	navX.zeroYaw()
+	periodic {
+		if (abs(navX.angle - angle) >= TURN_TOLERANCE) {
+			DriveBase.tankDrive(speed, -speed)
+		} else {
+			stop()
+		}
 	}
 }
