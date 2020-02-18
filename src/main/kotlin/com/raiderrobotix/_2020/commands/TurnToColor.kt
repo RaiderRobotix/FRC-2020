3ce35877373a7f5d2157d2419d3c03da99fa09ff
hybras <24651269+hybras@users.noreply.github.com>
2020-02-18 15:41:29 -0500

    Assert nonnullabilitty of selected color

--- src/main/kotlin/com/raiderrobotix/_2020/commands/TurnToColor.kt
+++ src/main/kotlin/com/raiderrobotix/_2020/commands/TurnToColor.kt
@@ -5,7 +5,7 @@ import com.raiderrobotix._2020.util.WheelColor
 import org.team2471.frc.lib.coroutines.periodic
 import org.team2471.frc.lib.framework.use
 
-suspend fun turnPanelToColor(color: WheelColor = WheelColor.selectedColor) = use(ColorWheel) {
+suspend fun turnPanelToColor(color: WheelColor = WheelColor.selectedColor!!) = use(ColorWheel) {
 	periodic {
 		if (WheelColor.color != color)
 			ColorWheel.motor.set(0.1)
