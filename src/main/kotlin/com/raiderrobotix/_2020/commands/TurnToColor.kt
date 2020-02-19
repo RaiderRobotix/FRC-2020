3b2734a25825258fe1e3d3d2b55f6f867e3cc278
raiderrobotix25 <frcteam25@gmail.com>
2020-02-19 15:45:54 -0500

    Merge in turn function

--- src/main/kotlin/com/raiderrobotix/_2020/commands/TurnToColor.kt
+++ src/main/kotlin/com/raiderrobotix/_2020/commands/TurnToColor.kt
@@ -5,10 +5,10 @@ import com.raiderrobotix._2020.util.WheelColor
 import org.team2471.frc.lib.coroutines.periodic
 import org.team2471.frc.lib.framework.use
 
-suspend fun turnPanelToColor(color: WheelColor = WheelColor.selectedColor!!) = use(ColorWheel) {
+suspend fun turnPanelToColor(color: WheelColor) = use {
 	periodic {
 		if (WheelColor.color != color)
-			ColorWheel.motor.set(0.1)
+		ColorWheel.speed = 0.1
 		else stop()
 	}
 }
