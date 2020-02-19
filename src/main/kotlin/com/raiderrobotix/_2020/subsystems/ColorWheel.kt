b9f90459ea01a01d1e211a5becfe9d887127b31f
raiderrobotix25 <frcteam25@gmail.com>
2020-02-19 15:45:34 -0500

    Merge in colorWheel subsystem

--- src/main/kotlin/com/raiderrobotix/_2020/subsystems/ColorWheel.kt
+++ src/main/kotlin/com/raiderrobotix/_2020/subsystems/ColorWheel.kt
@@ -4,5 +4,18 @@ import edu.wpi.first.wpilibj.Spark
 import org.team2471.frc.lib.framework.Subsystem
 
 object ColorWheel : Subsystem("ColorWheel") {
-	val motor = Spark(6)
+	private const val channel = 6
+	internal val wheel = Spark(6)
+    
+	var speed: Double
+		set(it) {
+			wheel.set(it)
+			
+		}
+		get() = (wheel.get())
+	
+	override fun reset() {
+		speed = 0.0
+	}
+	
 }
