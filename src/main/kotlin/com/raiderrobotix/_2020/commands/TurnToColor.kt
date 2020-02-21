2dd2bede458cdc34b07c174b3636d764ae9c96fa
hybras <24651269+hybras@users.noreply.github.com>
2020-02-21 17:41:19 -0500

    Fetch selected color from Driverstation

--- src/main/kotlin/com/raiderrobotix/_2020/commands/TurnToColor.kt
+++ src/main/kotlin/com/raiderrobotix/_2020/commands/TurnToColor.kt
@@ -2,13 +2,33 @@ package com.raiderrobotix._2020.commands
 
 import com.raiderrobotix._2020.subsystems.ColorWheel
 import com.raiderrobotix._2020.subsystems.ColorWheel.WheelColor
+import com.raiderrobotix._2020.subsystems.ColorWheel.color
+import edu.wpi.first.wpilibj.DriverStation
 import org.team2471.frc.lib.coroutines.periodic
 import org.team2471.frc.lib.framework.use
 
-suspend fun turnPanelToColor(color: WheelColor = ColorWheel.color!!) = use {
-	periodic {
-		if (ColorWheel.color != color)
-			ColorWheel.wheel.speed = 0.5
-		else stop()
+suspend fun positionControl() {
+	val endColor = when (
+		try {
+			DriverStation.getInstance().gameSpecificMessage[0] // Get first character
+		} catch (e: Exception) {
+			'\u0000' // If string is null of empty, default to this
+		}.toLowerCase()) {
+		'r' -> WheelColor.Red
+		'b' -> WheelColor.Cyan
+		'g' -> WheelColor.Green
+		'y' -> WheelColor.Yellow
+		else -> {
+			println("Color not received from Driver Station")
+			return
+		}
 	}
+	use(ColorWheel) {
+		periodic {
+			if (color != endColor) {
+				ColorWheel.wheel.speed = 0.4
+			} else stop()
+		}
+	}
+	
 }
