09f95683569f66da4275fcc079c517a9b97609bb
hybras <24651269+hybras@users.noreply.github.com>
2020-02-24 14:30:15 -0500

    Remove Use guard

--- src/main/kotlin/com/raiderrobotix/_2020/commands/colorwheel/TurnToColor.kt
+++ src/main/kotlin/com/raiderrobotix/_2020/commands/colorwheel/TurnToColor.kt
@@ -6,7 +6,6 @@ import com.raiderrobotix._2020.subsystems.ColorWheel.color
 import edu.wpi.first.wpilibj.DriverStation
 import org.team2471.frc.lib.coroutines.periodic
 import org.team2471.frc.lib.coroutines.suspendUntil
-import org.team2471.frc.lib.framework.use
 
 suspend fun positionControl() {
 	val endColor = when (
@@ -21,18 +20,16 @@ suspend fun positionControl() {
 		'Y' -> WheelColor.Yellow
 		else -> error("Color not received from Driver Station")
 	}
-	use(ColorWheel) {
-		ColorWheel.wheel.speed = 0.4
-		suspendUntil { color == endColor }
-		ColorWheel.wheel.speed = -0.35
-		var periods = 50
-		periodic {
-			periods--
-			if (periods == 0) stop()
-		}
-		ColorWheel.wheel.speed = 0.0
-		
+	ColorWheel.wheel.speed = 0.4
+	suspendUntil { color == endColor }
+	ColorWheel.wheel.speed = -0.35
+	var periods = 50
+	periodic {
+		periods--
+		if (periods == 0) stop()
 	}
+	ColorWheel.wheel.speed = 0.0
+	
 	
 }
 
