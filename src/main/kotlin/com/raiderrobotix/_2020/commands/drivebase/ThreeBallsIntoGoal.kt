4aebeb75572fe169069debcd2183cd7c863d37c3
raiderrobotix25 <frcteam25@gmail.com>
2020-02-26 14:56:58 -0500

    Rewrite auton to queue and fire balls

--- src/main/kotlin/com/raiderrobotix/_2020/commands/drivebase/ThreeBallsIntoGoal.kt
+++ src/main/kotlin/com/raiderrobotix/_2020/commands/drivebase/ThreeBallsIntoGoal.kt
@@ -5,6 +5,7 @@ import com.raiderrobotix._2020.subsystems.Intake
 import com.raiderrobotix._2020.subsystems.Shooter
 import kotlinx.coroutines.cancelAndJoin
 import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import org.team2471.frc.lib.coroutines.suspendUntil
 
@@ -16,20 +17,29 @@ suspend fun threeBallsIntoGoal() = coroutineScope {
 	
 	val readyShooter = launch {
 		Shooter.speed = 1.0
-		suspendUntil { Shooter.speed >= 0.9 }
+		suspendUntil { Shooter.speed >= 0.15 }
 	}
 	
 	val queueBall = launch {
-		Intake.speed = 0.5
-		suspendUntil { !Intake.ShooterBreaker.get() }
-		Intake.speed = 0.0
+		Intake.speed = 1.0
+		launch {
+			while(true) {
+				Intake.outer.speed = 0.6
+				suspendUntil { !Intake.IntakeBreaker.get() }
+				Intake.outer.speed = 0.0
+				suspendUntil { Intake.IntakeBreaker.get() }
+			}
+		}
+		// suspendUntil { !Intake.ShooterBreaker.get() }
+		// suspendUntil { Intake.ShooterBreaker.get() }
 	}
-	
+	delay(10 * 1000)
 	readyShooter.join()
 	queueBall.join()
-	
-	DriveBase.speed = 0.6
-	suspendUntil { DriveBase.averageDistance > 12 * 5 || Shooter.Ultrasound() <= 12 * 5 }
-	DriveBase.speed = 0.0
+	Shooter.reset()
+	Intake.reset()
+	// DriveBase.speed = 0.6
+	// suspendUntil { DriveBase.averageDistance > 12 * 5 || Shooter.Ultrasound() <= 12 * 5 }
+	// DriveBase.speed = 0.0
 	prints.cancelAndJoin()
 }
