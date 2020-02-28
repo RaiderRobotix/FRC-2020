dc5e4e96e9502671a9f0ab131527fc07f6d96396
hybras <24651269+hybras@users.noreply.github.com>
2020-02-27 20:10:00 -0500

    Add turn to limelight target function

--- src/main/kotlin/com/raiderrobotix/_2020/commands/drivebase/Turn.kt
+++ src/main/kotlin/com/raiderrobotix/_2020/commands/drivebase/Turn.kt
@@ -1,9 +1,12 @@
 package com.raiderrobotix._2020.commands.drivebase
 
 import com.raiderrobotix._2020.subsystems.DriveBase
+import com.raiderrobotix._2020.util.LimeLight
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
 import org.team2471.frc.lib.coroutines.periodic
 import org.team2471.frc.lib.framework.use
 import kotlin.math.abs
+import kotlin.math.sign
 
 private const val TURN_TOLERANCE = 1.0 // TODO
 
@@ -18,6 +21,21 @@ suspend fun turn(angle: Double, speed: Double) = use(DriveBase) {
 			DriveBase.tankDrive(speed, -speed)
 		} else {
 			stop()
+		}
+	}
+}
+
+suspend fun centerLimelight() = use(DriveBase) {
+	LimeLight.ledMode = LimeLight.LedMode.on
+	val speed = 0.1
+	periodic {
+		if (abs(LimeLight.x) <= TURN_TOLERANCE) {
+			stop()
+			SmartDashboard.putBoolean("Centered", true)
+		} else {
+			SmartDashboard.putBoolean("Centered", false)
+			val sgn = sign(LimeLight.x)
+			DriveBase.tankDrive(speed * sgn, -speed * sgn)
 		}
 	}
 }
