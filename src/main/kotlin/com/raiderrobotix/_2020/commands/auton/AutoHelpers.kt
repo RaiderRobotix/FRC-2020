7bdd20a846e3d10fc4e1dd0a37927aae2feacae9
hybras <24651269+hybras@users.noreply.github.com>
2020-03-01 11:55:28 -0500

    Remove reference to outer CoroutineScope

--- src/main/kotlin/com/raiderrobotix/_2020/commands/auton/AutoHelpers.kt
+++ src/main/kotlin/com/raiderrobotix/_2020/commands/auton/AutoHelpers.kt
@@ -1,10 +1,9 @@
 package com.raiderrobotix._2020.commands.auton
 
 import com.raiderrobotix._2020.subsystems.Intake
-import kotlinx.coroutines.coroutineScope
 import org.team2471.frc.lib.coroutines.suspendUntil
 
-suspend fun queueBall(numBalls: Int) = coroutineScope {
+suspend fun queueBall(numBalls: Int) {
 	Intake.speed = 1.0
 	repeat(numBalls) {
 		Intake.outer.speed = 0.6
