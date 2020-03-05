aa45bd202b9ad497817fe84ca2fcf1d58d69b293
hybras <24651269+hybras@users.noreply.github.com>
2020-03-04 19:20:19 -0500

    Halt shooter after firing

--- src/main/kotlin/com/raiderrobotix/_2020/commands/auton/fireNBalls.kt
+++ src/main/kotlin/com/raiderrobotix/_2020/commands/auton/fireNBalls.kt
@@ -1,19 +1,13 @@
 package com.raiderrobotix._2020.commands.auton
 
-import com.raiderrobotix._2020.subsystems.DriveBase
 import com.raiderrobotix._2020.subsystems.Intake
 import com.raiderrobotix._2020.subsystems.Shooter
-import kotlinx.coroutines.cancelAndJoin
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import org.team2471.frc.lib.coroutines.suspendUntil
 
 suspend fun fireNBalls(numBalls: Int) = coroutineScope {
-	val prints = launch {
-		Intake.default()
-		Shooter.default()
-	}
 	
 	val readyShooter = launch {
 		Shooter.speed = 1.0
@@ -23,6 +17,6 @@ suspend fun fireNBalls(numBalls: Int) = coroutineScope {
 	delay(10 * 1000)
 	readyShooter.join()
 	Shooter.reset()
+	Shooter.speed = 0.0
 	Intake.reset()
-	prints.cancelAndJoin()
 }
