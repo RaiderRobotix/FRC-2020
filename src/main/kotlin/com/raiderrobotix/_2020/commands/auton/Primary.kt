6c074d52305d675d33163b90190c51905c5fd019
hybras <24651269+hybras@users.noreply.github.com>
2020-03-06 17:41:16 -0500

    Remove robot print statements from autonomous mode

--- src/main/kotlin/com/raiderrobotix/_2020/commands/auton/Primary.kt
+++ src/main/kotlin/com/raiderrobotix/_2020/commands/auton/Primary.kt
@@ -3,16 +3,8 @@ package com.raiderrobotix._2020.commands.auton
 import com.raiderrobotix._2020.commands.drivebase.drive
 import com.raiderrobotix._2020.subsystems.DriveBase
 import com.raiderrobotix._2020.subsystems.Intake
-import com.raiderrobotix._2020.subsystems.Shooter
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.cancelAndJoin
-import org.team2471.frc.lib.coroutines.meanlibLaunch
 
 suspend fun primaryAuton() {
-	val prints = GlobalScope.meanlibLaunch {
-		Intake.default()
-		Shooter.default()
-	}
 //	fireNBalls(3)
 //	drive(3.0, -0.2) // input correct distance
 //	turn(-15.0, 0.2)
@@ -23,5 +15,4 @@ suspend fun primaryAuton() {
 //	suspendUntil { !Intake.ShooterBreaker.get() }
 	Intake.speed = 0.0
 	DriveBase.speed = 0.0
-	prints.cancelAndJoin()
 }
