bf6b491f492b6306c45f2f67cb4be78a222a626d
hybras <24651269+hybras@users.noreply.github.com>
2020-03-07 14:20:02 -0500

    Revert "Restructure loop and delays in fireBalls"
    This reverts commit dab4fd6f4918f32e9dd0a5d16cf52ef38615369d.

--- src/main/kotlin/com/raiderrobotix/_2020/commands/auton/fireNBalls.kt
+++ src/main/kotlin/com/raiderrobotix/_2020/commands/auton/fireNBalls.kt
@@ -9,16 +9,20 @@ import org.team2471.frc.lib.coroutines.suspendUntil
 suspend fun fireNBalls(numBalls: Int) = coroutineScope {
 	Shooter.speed = 1.0
 	delay(1500)
+	Intake.upper.speed = 1.0
+	Intake.lower.speed = -1.0
+	Intake.outer.speed = 0.6
 	repeat(numBalls) {
-		Intake.upper.speed = 1.0
-		Intake.lower.speed = -1.0
-		Intake.outer.speed = 0.6
 		suspendUntil { !Intake.ShooterBreaker.input.get() }
 		suspendUntil { Intake.ShooterBreaker.input.get() }
-		delay(1500)
 		Intake.upper.speed = 0.0
 		Intake.lower.speed = 0.0
 		Intake.outer.speed = 0.0
+		delay(1500)
+		Intake.upper.speed = 1.0
+		Intake.lower.speed = -1.0
+		Intake.outer.speed = 0.6
+		
 	}
 	Shooter.reset()
 	Intake.reset()
