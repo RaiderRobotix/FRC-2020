9b0299a13cb31a8c33d9d421d03515ddb570d85a
hybras <24651269+hybras@users.noreply.github.com>
2020-03-07 13:18:04 -0500

    Make sure intake waits properly while firing

--- src/main/kotlin/com/raiderrobotix/_2020/commands/auton/fireNBalls.kt
+++ src/main/kotlin/com/raiderrobotix/_2020/commands/auton/fireNBalls.kt
@@ -3,17 +3,26 @@ package com.raiderrobotix._2020.commands.auton
 import com.raiderrobotix._2020.subsystems.Intake
 import com.raiderrobotix._2020.subsystems.Shooter
 import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
 import org.team2471.frc.lib.coroutines.suspendUntil
 
 suspend fun fireNBalls(numBalls: Int) = coroutineScope {
 	Shooter.speed = 1.0
+	delay(1500)
 	Intake.upper.speed = 1.0
 	Intake.lower.speed = -1.0
 	Intake.outer.speed = 0.6
 	repeat(numBalls) {
 		suspendUntil { !Intake.ShooterBreaker.input.get() }
 		suspendUntil { Intake.ShooterBreaker.input.get() }
-		suspendUntil { Shooter.speed >= 0.9 }
+		Intake.upper.speed = 0.0
+		Intake.lower.speed = 0.0
+		Intake.outer.speed = 0.0
+		delay(1500)
+		Intake.upper.speed = 1.0
+		Intake.lower.speed = -1.0
+		Intake.outer.speed = 0.6
+		
 	}
 	Shooter.reset()
 	Intake.reset()
