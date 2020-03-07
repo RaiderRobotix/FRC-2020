01243dd45c2f4d6568d68488652c71d1eb8bc95f
hybras <24651269+hybras@users.noreply.github.com>
2020-03-07 12:37:26 -0500

    Remove concurrency from queuballs

--- src/main/kotlin/com/raiderrobotix/_2020/commands/auton/AutoHelpers.kt
+++ src/main/kotlin/com/raiderrobotix/_2020/commands/auton/AutoHelpers.kt
@@ -2,36 +2,22 @@ package com.raiderrobotix._2020.commands.auton
 
 import com.raiderrobotix._2020.Robot
 import com.raiderrobotix._2020.subsystems.Intake
-import com.raiderrobotix._2020.subsystems.SensorOutput
 import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.cancelAndJoin
-import kotlinx.coroutines.launch
 import org.team2471.frc.lib.coroutines.meanlibLaunch
 import org.team2471.frc.lib.coroutines.suspendUntil
 
 suspend fun queueBall(numBalls: Int) {
 	Intake.upper.speed = 1.0
-	val outer = GlobalScope.launch {
-		repeat(numBalls) {
-			Intake.outer.speed = 0.6
-			suspendUntil { !Intake.IntakeBreaker.get() }
-			Intake.outer.speed = 0.0
-			suspendUntil { Intake.IntakeBreaker.get() }
-		}
+	Intake.lower.speed = -1.0
+	repeat(numBalls) {
+		Intake.outer.speed = 0.6
+		suspendUntil { !Intake.IntakeBreaker.input.get() }
+		Intake.outer.speed = 0.0
+		suspendUntil { Intake.IntakeBreaker.input.get() }
 	}
-	val lower = GlobalScope.launch {
-		repeat(numBalls) {
-			Intake.lower.speed = -1.0
-			suspendUntil { !Intake.StageBreaker.get() }
-			Intake.lower.speed = -0.9
-			suspendUntil { Intake.StageBreaker.get() }
-		}
-	}
-	suspendUntil { !Intake.ShooterBreaker.get() }
-	outer.cancelAndJoin()
-	lower.cancelAndJoin()
+	
 }
 
 internal fun launchPrints() = GlobalScope.meanlibLaunch {
-	Robot.subsystems.forEach { (it as SensorOutput).update() }
+	Robot.subsystems.forEach { it.default() }
 }
